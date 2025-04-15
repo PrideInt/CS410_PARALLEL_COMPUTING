732584dbf251358d9c8fc9708c8cba1044c7ffd2
Pride <20098992+PrideInt@users.noreply.github.com>
2025-04-14 20:00:20 -0400

    finished spatial filtering

--- spatialfiltering/spatialfilt.c
+++ spatialfiltering/spatialfilt.c
@@ -5,72 +5,98 @@ ARCHITECTURE HYPERCUBE(7);
 
 /* Written and refactored by Pride Yin */
 
-#define n 1280
-#define m 10
+/* Scaling down the dimensions of the image to 768x768 to avoid storage overflow complications */
+#define n 768
+/* #define n 3 */
 
-int inrows[m + 2][n + 2], outrows[m][n]; 
+/* Used to calculate n, given n = m * 2^7 */
+/* In this case: m = 6; 768 = 6 * 2^7 */
+#define m 6
+
+int inRows[m + 2][n + 2], outRows[m][n]; 
 int filter[3][3] = {1, 1, 1, 1, 1, 1, 1, 1, 1};
 
 MPI_Status status;
 
-int myrank, totalproc, blocksize;
+int rank, totalProcesses, size;
+int i, j, k;
 
-void input_image() {
+int origin, destination;
+int tag;
+
+void inputImage() {
+    /* Overflow issues hinge on the size of this image. However because
+       we are only on process 0, we can limit the amount of memory allocated
+       when in MPI mode utilizing multicomputer topology as to avoid duplication
+       as opposed to keeping this data structure global.
+    */
     int image[n + 2][n + 2];
 
-    for (k = 0; k < totalproc; k++) { 
-        dest = k;
+    for (i = 0; i < n + 2; i++) {
+        for (j = 0; j < n + 2; j++) {
+            image[i][j] = rand() % 256;
+        }
+    }
+
+    for (k = 0; k < totalProcesses; k++) { 
+        destination = k;
         tag = 1;
 
-        MPI_Send(&image[k*m][0], blocksize, MPI_INT, dest, tag, MPI_COMM_WORLD);
-    } 
+        MPI_Send(&image[k * m][0], size, MPI_INT, destination, tag, MPI_COMM_WORLD);
+    }
 }
 
 main() {
     MPI_Init();
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-    MPI_Comm_size(MPI_COMM_WORLD, &totalproc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &totalProcesses);
 
-    blocksize = (m + 2) * (n + 2);
+    size = (m + 2) * (n + 2);
 
-    if (myrank == 0) input_image();
-
-    source = 0;
+    if (rank == 0) {
+        inputImage();
+    }
+    origin = 0;
     tag = 1;
 
-    MPI_Recv(&inrows[0][0], blocksize, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
+    MPI_Recv(&inRows[0][0], size, MPI_INT, origin, tag, MPI_COMM_WORLD, &status);
     
+    /* Filter mask */
     for (i = 1; i <= m; i++) {
         for (j = 1; j <= n; j++) {
-            outrows[i - 1, j - 1] = 
-                (int) ((filter[0,0]*inrows[i-1,j-1] 
-                + filter[0,1]*inrows[i-1,j]
-                + filter[0,2]*inrows[i-1,j+1]
-                + filter[1,0]*inrows[i,j-1]
-                + filter[1,1]*inrows[i,j]
-                + filter[1,2]*inrows[i,j+1]
-                + filter[2,0]*inrows[i+1,j-1]
-                + filter[2,1]*inrows[i+1,j]
-                + filter[2,2]*inrows[i+1,j+1]) / 9);
+            outRows[i - 1, j - 1] = 
+                (int) ((filter[0, 0] * inRows[i - 1, j - 1] 
+                + filter[0, 1] * inRows[i - 1, j]
+                + filter[0, 2] * inRows[i - 1, j + 1]
+                + filter[1, 0] * inRows[i, j - 1]
+                + filter[1, 1] * inRows[i, j]
+                + filter[1, 2] * inRows[i, j + 1]
+                + filter[2, 0] * inRows[i + 1, j - 1]
+                + filter[2, 1] * inRows[i + 1, j]
+                + filter[2, 2] * inRows[i + 1, j + 1]) / 9);
         }
     }
-    blocksize = m * n;
+    size = m * n;
 
-    dest = 0;
+    destination = 0;
     tag = 2;
 
-    MPI_Send(&outrows[0][0], blocksize, MPI_INT, dest, tag, MPI_COMM_WORLD);
+    MPI_Send(&outRows[0][0], size, MPI_INT, destination, tag, MPI_COMM_WORLD);
 
-    if (myrank == 0) { 
-        for (k = 0; k < totalproc; k++) { 
-            source = k;
+    if (rank == 0) { 
+        for (k = 0; k < totalProcesses; k++) { 
+            origin = k;
             tag = 2;
 
-            MPI_Recv(&outrows[0][0], blocksize, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
+            MPI_Recv(&outRows[0][0], size, MPI_INT, origin, tag, MPI_COMM_WORLD, &status);
 
             for (i = 0; i < m; i++) {
                 for (j = 0; j <= n; j++) {
-                    cout << outrows[i][j];
+                    /* Only printing out the first 15 values of each row to avoid line buffer overflow */
+
+                    if (j <= 15) {
+                        cout << outRows[i][j];
+                    }
                 }
                 cout << endl; 
             }
